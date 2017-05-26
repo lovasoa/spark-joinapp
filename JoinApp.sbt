fdcd2960390804bf7fa69ad4986986c08eb836eb
Ophir LOJKINE <pere.jobs@gmail.com>
2017-05-26 17:48:28 +0300

    Add bloom filter

--- JoinApp.sbt
+++ JoinApp.sbt
@@ -1,10 +1,13 @@
 name := "JoinApp"
- 
+
 version := "1.0"
 
 scalaVersion := "2.11.8"
 
+resolvers += "jitpack" at "https://jitpack.io"
+
 libraryDependencies ++= Seq(
     "org.apache.spark" %% "spark-core" % "2.1.0",
-    "org.apache.spark" %% "spark-sql" % "2.1.0"
+    "org.apache.spark" %% "spark-sql" % "2.1.0",
+    "com.github.lovasoa" % "BloomFilter" % "2.2.1"
 )
---
+++ src/main/scala/BloomFilterAggregator.scala
@@ -0,0 +1,26 @@
+import org.apache.spark.sql.expressions.Aggregator
+import org.apache.spark.sql.Encoder
+import org.apache.spark.sql.Encoders
+import com.github.lovasoa.bloomfilter.BloomFilter
+
+object BloomFilterAggregator extends Aggregator[Object, BloomFilter, BloomFilter] {
+  // A zero value for this aggregation. Should satisfy the property that any b + zero = b
+  def zero: BloomFilter = new BloomFilter(1000000)
+  // Combine two values to produce a new value. For performance, the function may modify `buffer`
+  // and return it instead of constructing a new object
+  def reduce(buffer: BloomFilter, value: Object): BloomFilter = {
+    buffer.add(value)
+    buffer
+  }
+  // Merge two intermediate values
+  def merge(b1: BloomFilter, b2: BloomFilter): BloomFilter = {
+    b1.merge(b2)
+    b1
+  }
+  // Transform the output of the reduction
+  def finish(reduction: BloomFilter): BloomFilter = reduction
+  // Specifies the Encoder for the intermediate value type
+  def bufferEncoder: Encoder[BloomFilter] = Encoders.kryo[BloomFilter]
+  // Specifies the Encoder for the final output value type
+  def outputEncoder: Encoder[BloomFilter] = bufferEncoder
+}
