aa079426a49a92fd3a029de4d19f7f22e168722b
Ophir LOJKINE <pere.jobs@gmail.com>
2017-06-12 22:06:14 +0300

    Move all argument parsing to AppConfig

--- src/main/scala/AppConfig.scala
+++ src/main/scala/AppConfig.scala
@@ -7,3 +7,45 @@ case class AppConfig(
   tables: Seq[String] = Seq(),
   debug: Boolean = false
 )
+
+object AppConfig {
+  def parser =
+    new scopt.OptionParser[AppConfig]("joinapp") {
+    head("joinapp", "0.1")
+
+    opt[Unit]('q', "query").action( (_, c) =>
+      c.copy(query = true) ).text("execute the query")
+
+    opt[Unit]('b', "bloom").action( (_, c) =>
+      c.copy(query = true, bloom = true)
+    ).text("use a bloom-filtered join")
+
+    opt[Unit]("sql").action( (_, c) =>
+      c.copy(bloom = false) ).text("use a simple SparkSQL query")
+
+    opt[Double]('e', "error-rate").action( (x, c) =>
+      c.copy(query=true, bloom=true, errorRate = x)
+    ).text("error rate to use in bloom filters")
+
+    opt[Unit]("debug").action( (_, c) =>
+      c.copy(debug = true) ).text("print debug informations while doing the query")
+
+    opt[Unit]('c', "convert").action( (_, c) =>
+      c.copy(convert = true) ).text("convert files from CSV to parquet")
+
+    opt[String]('s', "source-path").action( (x, c) =>
+      c.copy(sourcePath = x) ).text("convert files from CSV to parquet")
+
+    arg[String]("<table>...").unbounded().optional().action( (x, c) =>
+      c.copy(tables = c.tables :+ x) ).text("tables to convert")
+  }
+
+  def parse(args: Array[String]) : Option[AppConfig] =
+    parser.parse(args, AppConfig())
+
+  def parseOrDie(args: Array[String]) : AppConfig =
+    parse(args) match {
+      case Some(parsed) => parsed
+      case None => {System.exit(1); AppConfig()}
+    }
+}
--- src/main/scala/JoinApp.scala
+++ src/main/scala/JoinApp.scala
@@ -10,43 +10,9 @@ object Main {
   val logger = Logger.getLogger(Main.getClass)
   var conf : AppConfig = AppConfig()
 
-  private val appConfigParser =
-    new scopt.OptionParser[AppConfig]("joinapp") {
-    head("joinapp", "0.1")
-
-    opt[Unit]('q', "query").action( (_, c) =>
-      c.copy(query = true) ).text("execute the query")
-
-    opt[Unit]('b', "bloom").action( (_, c) =>
-      c.copy(query = true, bloom = true)
-    ).text("use a bloom-filtered join")
-
-    opt[Unit]("sql").action( (_, c) =>
-      c.copy(bloom = false) ).text("use a simple SparkSQL query")
-
-    opt[Double]('e', "error-rate").action( (x, c) =>
-      c.copy(query=true, bloom=true, errorRate = x)
-    ).text("error rate to use in bloom filters")
-
-    opt[Unit]("debug").action( (_, c) =>
-      c.copy(debug = true) ).text("print debug informations while doing the query")
-
-    opt[Unit]('c', "convert").action( (_, c) =>
-      c.copy(convert = true) ).text("convert files from CSV to parquet")
-
-    opt[String]('s', "source-path").action( (x, c) =>
-      c.copy(sourcePath = x) ).text("convert files from CSV to parquet")
-
-    arg[String]("<table>...").unbounded().optional().action( (x, c) =>
-      c.copy(tables = c.tables :+ x) ).text("tables to convert")
-  }
-
   def main(args: Array[String]) {
     // Parse command-line
-    appConfigParser.parse(args, AppConfig()) match {
-      case Some(parsed) => conf = parsed
-      case None => System.exit(1)
-    }
+    conf = AppConfig.parseOrDie(args)
 
     spark = SparkSession.builder()
       .master("yarn")
