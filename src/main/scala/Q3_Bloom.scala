5fe1245628b054009f7dc4356b25bfeb8238e01b
Ophir LOJKINE <pere.jobs@gmail.com>
2017-06-15 01:30:21 +0300

    Add job descriptions

--- src/main/scala/Q3_Bloom.scala
+++ src/main/scala/Q3_Bloom.scala
@@ -22,6 +22,7 @@ class Q3_Bloom extends Q3 {
     filteredOrders.cache()
 
     // Getting an fast approximation of the number of distinct order keys
+    sc.setJobGroup("countApprox", "Estimationg the number of elements in the filtered small table")
     var cntPartial = filteredOrders.rdd.countApprox(timeout=4000, confidence=0)
     val interval = if (cntPartial.initialValue.confidence < 0.75) {
       logger.warn(s"Count interval with low confidence ($cntPartial). Waiting.")
@@ -50,6 +51,7 @@ class Q3_Bloom extends Q3 {
 
     if (Main.conf.debug) debug(lineitem, checkInFilter)
 
+    sc.setJobGroup("join", "Filtering the large table and computing the final join")
     spark.read.table("lineitem")
       .filter($"l_shipdate" > "1995-03-15" && checkInFilter($"l_orderkey"))
       .join(filteredOrders, $"l_orderkey" === $"o_orderkey")
--- src/main/scala/TreeBloom.scala
+++ src/main/scala/TreeBloom.scala
@@ -1,12 +1,14 @@
 import org.apache.spark.util.sketch.BloomFilter
 import org.apache.spark.sql._
 import org.apache.spark.sql.catalyst.InternalRow
+import Main.sc
 
 object TreeBloom {
   // Implements bloom filter using treeAggregate instead of aggregate
   // See https://issues.apache.org/jira/browse/SPARK-21039
   def bloomFilter(singleCol: DataFrame, expectedNumItems:Long, fpp:Double): BloomFilter = {
     val zero = BloomFilter.create(expectedNumItems, fpp)
+    sc.setJobGroup("bloomFilter", "Creation of a bloom filter")
     singleCol.queryExecution.toRdd.treeAggregate(zero)(
       (filter: BloomFilter, row: InternalRow) => {
         filter.putLong(row.getInt(0))
