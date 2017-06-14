57b51fced518e8b718b0c59657e5fdd391e2bc82
Ophir LOJKINE <pere.jobs@gmail.com>
2017-06-15 01:38:00 +0300

    Lower the confidence needed for accepting a count
    Accept a count if it comes from 5% of the partitions or more

--- src/main/scala/Q3_Bloom.scala
+++ src/main/scala/Q3_Bloom.scala
@@ -24,7 +24,7 @@ class Q3_Bloom extends Q3 {
     // Getting an fast approximation of the number of distinct order keys
     sc.setJobGroup("countApprox", "Estimationg the number of elements in the filtered small table")
     var cntPartial = filteredOrders.rdd.countApprox(timeout=4000, confidence=0)
-    val interval = if (cntPartial.initialValue.confidence < 0.75) {
+    val interval = if (cntPartial.initialValue.confidence < 0.05) {
       logger.warn(s"Count interval with low confidence ($cntPartial). Waiting.")
       cntPartial.getFinalValue()
     } else { cntPartial.initialValue }
