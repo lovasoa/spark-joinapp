8c9eb49363a1c65e03459007f50b06d1bf321c03
Ophir LOJKINE <pere.jobs@gmail.com>
2017-06-15 03:46:05 +0300

    try with synchronized

--- src/main/scala/Q3_Bloom.scala
+++ src/main/scala/Q3_Bloom.scala
@@ -25,7 +25,7 @@ class Q3_Bloom extends Q3 {
     sc.setJobGroup("countApprox", "Estimating the number of elements in the filtered small table")
     @volatile var cntPartial = filteredOrders.rdd.countApprox(timeout=1000, confidence=0.1)
     while (cntPartial.initialValue.low < 0.1 * cntPartial.initialValue.high
-          && !cntPartial.isInitialValueFinal) {
+          && !cntPartial.isInitialValueFinal) synchronized {
       // If we have an order of magnitude of difference between low and high
       // then wait for better results.
       logger.warn(s"""Count interval with low confidence ($cntPartial).
